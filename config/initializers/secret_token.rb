ffd2764e1cdeb709df6aa2b9a8e066dd8bd27313
Saurabh <saurabh@lightbank.com>
2012-04-12 19:53:00 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '5e6bbb33376faa5a1d4ce27e88186a90905070904b9cbaf23ec9544b064aebf7adf992d590b45390630d1c8d6caca741792ca7d5d9686fc9dcad9110c7a0396f'
